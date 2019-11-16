f8c1db14c177d93274a8bdfc7f5d0c9a31741ade
Michael <mobrien@colgate.edu>
2019-11-16 15:44:36 -0500

    made divs for info page and added some detail to the individual page

--- src/InfoPage.js
+++ src/InfoPage.js
@@ -1,17 +1,35 @@
 import React, { Component } from "react";
+import { Grid, Segment, Container } from "semantic-ui-react";
 
 class InfoPage extends Component {
   handleClick = () => {
     this.props.history.push("/softwareeng");
-    console.log("page change");
+    // console.log("page change");
   };
 
   render() {
     return (
-      <div>
-        {console.log("rendered InfoPage")}
-        <button onClick={() => this.handleClick()}>Click Me</button>
-      </div>
+      <>
+        <Grid centered columns={3}>
+          <Grid.Column computer={5}>
+            <Segment onClick={() => this.handleClick()}>
+              <Container textAlign="center">Software Engineering</Container>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column computer={5}>
+            <Segment>
+              <Container textAlign="center">Cybersecurity</Container>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column computer={5}>
+            <Segment>
+              <Container textAlign="center">Cloud Support</Container>
+            </Segment>
+          </Grid.Column>
+        </Grid>
+        {/* {console.log("rendered InfoPage")} */}
+        {/* <button onClick={() => this.handleClick()}>Click Me</button> */}
+      </>
     );
   }
 }
--- src/SoftwareEng.js
+++ src/SoftwareEng.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const SoftwareEng = () => {
-  return <div>{console.log("rendered SoftwareEng")}</div>;
+  return (
+    <div>
+      {console.log("rendered SoftwareEng")}
+      <p>
+        The field of software engineering entails Simplifying and improving the
+        lives of the users of their software applications by assessing needs,
+        desires and demands. Using backend and/or frontend languages to design
+        and develop mockups and models to illustrate software functionality.
+        Continuously learning and researching new tools to keep technology
+        skills current
+      </p>
+    </div>
+  );
 };
 
 export default SoftwareEng;
